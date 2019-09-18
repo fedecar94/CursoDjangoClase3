a392c4acf10d55a733bae9e4d91233921a9fcb0d
Federico Cardozo <lightfox.tekoreimante@gmail.com>
2019-09-18 19:47:10 -0400

    tercer commit

--- curso/settings.py
+++ curso/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'base',
     'blog',
+    'web',
 ]
 
 MIDDLEWARE = [
--- curso/urls.py
+++ curso/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('web.urls')),
     path('blog/', include('blog.urls')),
     path('', include('base.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
